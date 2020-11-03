71adfbb96d11028bbc4128a41ca2d99f1d1a9ebf
Anthony Brown <ant.brown@cgi.com>
2020-11-03 15:02:01 +0200

    Added fields to contact info page

--- components/notification/Contact.tsx
+++ components/notification/Contact.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useI18n } from "next-localization";
+import { TextInput } from "hds-react";
 import { RootState } from "../../state/reducers";
 import Location from "./Location";
 
@@ -12,9 +13,21 @@ const Contact = (): ReactElement => {
   return (
     <div>
       <h1>{`${currentPage} ${i18n.t("notification.contact.title")}`}</h1>
-      <div>ADDRESS</div>
+
+      <h2>{i18n.t("notification.contact.address.title")}</h2>
+      <TextInput id="streetAddress" label={i18n.t("notification.contact.address.streetAddress")} required />
+      <TextInput id="postalCode" label={i18n.t("notification.contact.address.postalCode")} required />
+      <TextInput id="postalOffice" label={i18n.t("notification.contact.address.postalOffice")} required />
+
       <Location />
-      <div>LINKS</div>
+
+      <h2>{i18n.t("notification.contact.contact.title")}</h2>
+      <TextInput id="phone" label={i18n.t("notification.contact.contact.phone")} />
+      <TextInput id="email" label={i18n.t("notification.contact.contact.email")} />
+
+      <h2>{i18n.t("notification.contact.link.title")}</h2>
+      <TextInput id="website" label={i18n.t("notification.contact.link.website")} />
+      <TextInput id="socialMedia" label={i18n.t("notification.contact.link.socialMedia")} />
     </div>
   );
 };
