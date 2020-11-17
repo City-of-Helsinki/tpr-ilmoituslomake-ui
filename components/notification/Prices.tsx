a9ba35bd0c84124d22bd1ba8e7207dc95214f54c
Anthony Brown <ant.brown@cgi.com>
2020-11-17 14:07:45 +0200

    Added more language-specific notification form fields

--- components/notification/Prices.tsx
+++ components/notification/Prices.tsx
@@ -2,6 +2,7 @@ import React, { Dispatch, ChangeEvent, ReactElement } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useI18n } from "next-localization";
 import { TextArea } from "hds-react";
+import { languageOptions } from "./InputLanguage";
 import { NotificationAction } from "../../state/actions/types";
 import { setNotificationPrice } from "../../state/actions/notification";
 import { RootState } from "../../state/reducers";
@@ -11,9 +12,10 @@ const Prices = (): ReactElement => {
   const dispatch = useDispatch<Dispatch<NotificationAction>>();
 
   const notification = useSelector((state: RootState) => state.notification.notification);
-  const {
-    price: { fi: priceFi },
-  } = notification;
+  const { price } = notification;
+
+  const notificationExtra = useSelector((state: RootState) => state.notification.notificationExtra);
+  const { inputLanguages } = notificationExtra;
 
   const updatePrice = (evt: ChangeEvent<HTMLTextAreaElement>) => {
     dispatch(setNotificationPrice({ [evt.target.name]: evt.target.value }));
@@ -22,15 +24,23 @@ const Prices = (): ReactElement => {
   return (
     <div className="formSection">
       <h2>{i18n.t("notification.prices.title")}</h2>
-      <TextArea
-        id="price"
-        className="formInput"
-        rows={6}
-        label={i18n.t("notification.prices.price.label")}
-        name="fi"
-        value={priceFi}
-        onChange={updatePrice}
-      />
+      <div className={inputLanguages.length > 1 ? "languageSection" : ""}>
+        {inputLanguages.length > 1 && <h3>{i18n.t("notification.prices.price.label")}</h3>}
+        {languageOptions.map((option) =>
+          inputLanguages.includes(option) ? (
+            <TextArea
+              id={`price_${option}`}
+              key={`price_${option}`}
+              className="formInput"
+              rows={6}
+              label={`${i18n.t("notification.prices.price.label")} ${i18n.t(`notification.inputLanguage.${option}`)}`}
+              name={option}
+              value={price[option] as string}
+              onChange={updatePrice}
+            />
+          ) : null
+        )}
+      </div>
     </div>
   );
 };
