3894abcce0cd5fc0eef8d39af00dce4afd780a5e
Ristomatti Karppanen <ristomatti.karppanen@cgi.com>
2020-11-19 16:23:58 +0200

    styling index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import React, { ReactElement } from "react";
+import { Button, IconArrowLeft, Card, Koros } from "hds-react";
 import { GetStaticProps } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import { useI18n } from "next-localization";
 import i18nLoader from "../utils/i18n";
 import Layout from "../components/Layout";
-
 import Header from "../components/common/Header";
+
+import styles from "./index.module.scss";
 
 const Main = (): ReactElement => {
   const i18n = useI18n();
@@ -18,7 +20,37 @@ const Main = (): ReactElement => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <div>HOME</div>
+      <div className={styles.korosTextBox}>
+        <h1>Pidetään yhdessä Helsingin parhaiden paikkojen tiedot ajantasalla!</h1>
+      </div>
+      <Koros flipHorizontal className={styles.koros} />
+      <div className={styles.infoTextBox}>
+        <h3>
+          My Helsinki Places kerää ja ylläpitää tietoa Helsingin kohteista paikallisten ja matkailijoiden hyödyksi. Tässä palvelussa voit ilmoittaa kohteet tiedot tai tehdä muutoksia jo olemassa oleviin kohdetietoihin.
+        </h3>
+      </div>
+      <div className={styles.infoLinkContainer}>
+        <div>Lue tietojen ilmoittamisesta</div>
+        <div>Lue saavutettavuusseloste</div>
+        <div>Ilmoita kohteiden tietoja</div>
+      </div>
+      <div>
+        <Card heading={"Haluatko lisätä uuden kohteen palveluun?"} text={"Ilmoita palveluun uusia toimipisteitä ja kohteita. Kirjaudu ensin ja ilmoita puuttuva kohde jo tänään."}>
+          <Button variant="secondary" onClick={() => (null)}>
+            {"Ilmoita uusi kohde"}
+          </Button>
+        </Card>
+        <Card heading={"Anna meille vinkki!"} text={"Mikäli huomasit puuttuvan, virheellisen tai vanhentuneen tiedon, tee palvelus ja ilmoita siitä meille. Voit tehdä ilmoituksen vaivattomasti rekisteröitymättä."}>
+          <Button variant="secondary" onClick={() => (null)}>
+            {"Vinkkaa meille"}
+          </Button>
+        </Card>
+        <Card heading={"Mistä tietoja kohteesta löytyy?"} text={"Tarkista, onko kohde jo ilmoitettu meille ja mitä tietoa siitä on olemassa."}>
+          <Button variant="secondary" onClick={() => (null)}>
+            {"Tarkista kohteen tiedot"}
+          </Button>
+        </Card>
+      </div>
       <div>
         <Link href="/forms/notification">{i18n.t("notification.title")}</Link>
       </div>
