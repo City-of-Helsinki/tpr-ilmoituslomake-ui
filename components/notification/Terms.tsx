636abac4d6c85f88e704de9c0d0cb849fbe6fa82
Anthony Brown <ant.brown@cgi.com>
2020-12-16 13:48:51 +0200

    Added terms of use section to notification form

--- components/notification/Terms.tsx
+++ components/notification/Terms.tsx
@@ -1,12 +1,27 @@
 import React, { ReactElement } from "react";
 import { useI18n } from "next-localization";
-import { Card } from "hds-react";
+import { Button, IconLinkExternal } from "hds-react";
 import styles from "./Terms.module.scss";
 
 const Terms = (): ReactElement => {
   const i18n = useI18n();
 
-  return <Card className={styles.terms} heading={i18n.t("notification.terms.heading")} text={i18n.t("notification.terms.text")} />;
+  const openTermsOfUse = () => {
+    window.open("https://www.myhelsinki.fi/fi/myhelsinki-places-palvelun-käyttöehdot/", "_blank");
+  };
+
+  return (
+    <div className={styles.terms}>
+      <div className="hds-notification__label">{i18n.t("notification.terms.heading")}</div>
+      <div className={styles.notice}>
+        <div>{i18n.t("notification.terms.text1")}</div>
+        <Button variant="supplementary" size="small" className={styles.creativeCommonsLink} iconRight={<IconLinkExternal />} onClick={openTermsOfUse}>
+          {i18n.t("notification.terms.link")}
+        </Button>
+        <div>{i18n.t("notification.terms.text2")}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Terms;
