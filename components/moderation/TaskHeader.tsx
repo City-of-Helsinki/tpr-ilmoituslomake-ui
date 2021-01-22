751088a1260f0e09d1d3f207da0727e7e8a4f2ff
Anthony Brown <ant.brown@cgi.com>
2021-01-22 14:03:20 +0200

    Allow change requests to be opened so moderator can modify them

--- components/moderation/TaskHeader.tsx
+++ components/moderation/TaskHeader.tsx
@@ -1,16 +1,19 @@
-import React, { ReactElement, useState } from "react";
-import { useSelector } from "react-redux";
+import React, { Dispatch, ReactElement, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { useI18n } from "next-localization";
 import { Button, IconArrowRight, IconArrowUndo, IconCheck, IconCross, IconTrash, Notification as HdsNotification } from "hds-react";
 import moment from "moment";
+import { ModerationStatusAction } from "../../state/actions/types";
 import { RootState } from "../../state/reducers";
-import { DATETIME_FORMAT, NotifierType, TaskType, Toast } from "../../types/constants";
+import { DATETIME_FORMAT, ModerationStatus, NotifierType, TaskType, Toast } from "../../types/constants";
 import { saveModeration } from "../../utils/save";
+import setModerationStatus from "../../utils/status";
 import TaskStatusLabel from "./TaskStatusLabel";
 import styles from "./TaskHeader.module.scss";
 
 const TaskHeader = (): ReactElement => {
   const i18n = useI18n();
+  const dispatchStatus = useDispatch<Dispatch<ModerationStatusAction>>();
 
   const currentUser = useSelector((state: RootState) => state.notification.user);
   const selectedTaskId = useSelector((state: RootState) => state.moderation.selectedTaskId);
@@ -26,17 +29,23 @@ const TaskHeader = (): ReactElement => {
   const modifiedTask = useSelector((state: RootState) => state.moderation.modifiedTask);
   const moderationExtra = useSelector((state: RootState) => state.moderation.moderationExtra);
   const {
+    photosSelected,
     changeRequest: { description, contact_details } = {},
     created_at,
     taskType,
     status,
     moderator: { fullName: moderatorName },
   } = moderationExtra;
+  const pageStatus = useSelector((state: RootState) => state.moderationStatus.pageStatus);
 
   const [toast, setToast] = useState<Toast>();
 
   const cleanupToast = () => {
     setToast(undefined);
+  };
+
+  const openForModifying = () => {
+    setModerationStatus(photosSelected, dispatchStatus);
   };
 
   return (
@@ -63,10 +72,22 @@ const TaskHeader = (): ReactElement => {
       {(taskType === TaskType.ChangeTip || taskType === TaskType.RemoveTip) && (
         <div className={styles.buttonRow}>
           <Button variant="secondary">{i18n.t("moderation.button.requestTranslation")}</Button>
-          <Button variant="secondary">{i18n.t("moderation.button.openForModifying")}</Button>
           <Button variant="secondary" iconRight={<IconArrowUndo />}>
             {i18n.t("moderation.button.rejectChangeRequest")}
           </Button>
+          {pageStatus !== ModerationStatus.Edited && (
+            <Button variant="secondary" onClick={openForModifying}>
+              {i18n.t("moderation.button.openForModifying")}
+            </Button>
+          )}
+          {pageStatus === ModerationStatus.Edited && (
+            <Button
+              iconRight={<IconArrowRight />}
+              onClick={() => saveModeration(currentUser, modifiedTaskId, modifiedTask, moderationExtra, setToast)}
+            >
+              {i18n.t("moderation.button.saveInformation")}
+            </Button>
+          )}
         </div>
       )}
 
---
+++ utils/status.ts
@@ -0,0 +1,49 @@
+import { Dispatch } from "react";
+import { ModerationStatusAction } from "../state/actions/types";
+import {
+  setPageStatus,
+  setModerationAddressStatus,
+  setModerationContactStatus,
+  setModerationLinkStatus,
+  setModerationLocationStatus,
+  setModerationLongDescriptionStatus,
+  setModerationNameStatus,
+  setModerationPhotoAltTextStatus,
+  setModerationPhotoStatus,
+  setModerationShortDescriptionStatus,
+  setModerationTagStatus,
+} from "../state/actions/moderationStatus";
+import { LANGUAGE_OPTIONS, ModerationStatus } from "../types/constants";
+import { Photo } from "../types/general";
+
+export const setModerationStatus = (photos: Photo[] = [], dispatch: Dispatch<ModerationStatusAction>): void => {
+  const newStatus = ModerationStatus.Edited;
+
+  dispatch(setPageStatus(newStatus));
+
+  dispatch(setModerationTagStatus(newStatus));
+  dispatch(setModerationAddressStatus("fi", { street: newStatus, postal_code: newStatus, post_office: newStatus }));
+  dispatch(setModerationAddressStatus("sv", { street: newStatus, postal_code: newStatus, post_office: newStatus }));
+  dispatch(setModerationLocationStatus(newStatus));
+  dispatch(setModerationContactStatus({ phone: newStatus }));
+  dispatch(setModerationContactStatus({ email: newStatus }));
+
+  LANGUAGE_OPTIONS.forEach((language) => {
+    dispatch(setModerationNameStatus({ [language]: newStatus }));
+    dispatch(setModerationShortDescriptionStatus({ [language]: newStatus }));
+    dispatch(setModerationLongDescriptionStatus({ [language]: newStatus }));
+    dispatch(setModerationLinkStatus({ [language]: newStatus }));
+  });
+
+  photos.forEach((photo, index) => {
+    dispatch(setModerationPhotoStatus(index, { url: newStatus }));
+    dispatch(setModerationPhotoStatus(index, { permission: newStatus }));
+    dispatch(setModerationPhotoStatus(index, { source: newStatus }));
+
+    LANGUAGE_OPTIONS.forEach((language) => {
+      dispatch(setModerationPhotoAltTextStatus(index, { [language]: newStatus }));
+    });
+  });
+};
+
+export default setModerationStatus;
