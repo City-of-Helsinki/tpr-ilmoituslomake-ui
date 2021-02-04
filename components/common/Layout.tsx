2f8eccec9d5706c4243ce99f37fc867f90cb73ee
Anthony Brown <ant.brown@cgi.com>
2021-02-04 15:10:47 +0200

    Bug fix for huge svg icons in next.js production mode

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, ReactElement } from "react";
+import React, { ReactNode, ReactElement, useEffect, useRef } from "react";
 import Footer from "./Footer";
 import styles from "./Layout.module.scss";
 
@@ -7,8 +7,18 @@ interface LayoutProps {
 }
 
 const Layout = ({ children }: LayoutProps): ReactElement => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // The layout is hidden during server-side rendering so that large svg icons are not shown momentarily (in production mode)
+    // Restore the proper layout styling after client-side rendering here
+    if (ref.current) {
+      ref.current.className = styles.layout;
+    }
+  });
+
   return (
-    <div className={styles.layout}>
+    <div className="hidden" ref={ref}>
       <div className={styles.left} />
       <div className={styles.main}>
         {children}
