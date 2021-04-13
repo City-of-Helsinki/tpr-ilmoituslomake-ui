ba2b55cb0d846c2c899017422ed96828801d6178
Anthony Brown <ant.brown@cgi.com>
2021-04-13 15:10:45 +0300

    Commented out some debug info

--- utils/request.ts
+++ utils/request.ts
@@ -3,8 +3,8 @@ import absoluteUrl from "next-absolute-url";
 
 export const getOrigin = (router: NextRouter): string => {
   const { origin } = absoluteUrl();
-  console.log("origin", origin);
-  console.log("basePath", router.basePath);
+  // console.log("origin", origin);
+  // console.log("basePath", router.basePath);
 
   return `${origin}${router.basePath}`;
 };
