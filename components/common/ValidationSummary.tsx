4e794ade476742a20269617c370d2d66d1ab5c56
Anthony Brown <ant.brown@cgi.com>
2021-09-02 11:00:31 +0300

    Changed validation error to get focus when shown for screen readers

--- components/common/ValidationSummary.tsx
+++ components/common/ValidationSummary.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect, useRef } from "react";
 import { useI18n } from "next-localization";
 import { Notification as HdsNotification } from "hds-react";
 import styles from "./ValidationSummary.module.scss";
@@ -9,9 +9,17 @@ interface ValidationSummaryProps {
 
 const ValidationSummary = ({ prefix }: ValidationSummaryProps): ReactElement => {
   const i18n = useI18n();
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.scrollIntoView();
+      ref.current.focus();
+    }
+  });
 
   return (
-    <div className={styles.validationSummary}>
+    <div className={styles.validationSummary} ref={ref} tabIndex={-1}>
       <HdsNotification size="default" className="formNotification" type="error" label={i18n.t(`${prefix}.message.validationFailed.title`)}>
         {i18n.t(`${prefix}.message.validationFailed.message`)}
       </HdsNotification>
