898687aa2a4ecdf800dedd69379688ec6285cf4e
Anthony Brown <ant.brown@cgi.com>
2021-11-24 14:05:11 +0200

    Further improved opening times info with proper layout and text

--- components/notification/OpeningTimesInfo.tsx
+++ components/notification/OpeningTimesInfo.tsx
@@ -4,26 +4,28 @@ import { useRouter } from "next/router";
 import { useI18n } from "next-localization";
 import moment from "moment";
 import { RootState } from "../../state/reducers";
+import { DATE_FORMAT, DATE_FORMAT_HAUKI, TIME_FORMAT, TIME_FORMAT_HAUKI } from "../../types/constants";
 import { OpeningTime } from "../../types/general";
 import getOrigin from "../../utils/request";
 import styles from "./OpeningTimesInfo.module.scss";
-import { DATE_FORMAT, DATE_FORMAT_HAUKI, TIME_FORMAT, TIME_FORMAT_HAUKI } from "../../types/constants";
 
 const OpeningTimesInfo = (): ReactElement => {
   const i18n = useI18n();
   const router = useRouter();
 
   const [openingTimes, setOpeningTimes] = useState<OpeningTime[]>([]);
+  const [startDateMoment, setStartDateMoment] = useState<moment.Moment>();
 
   const openingTimesId = useSelector((state: RootState) => state.notification.openingTimesId);
 
   const getOpeningTimesOnMount = async () => {
     if (openingTimesId > 0) {
       // Use this week's Monday as the start date, and fetch the next two weeks opening times
-      const startDateMoment = moment().isoWeekday(1);
-      const endDateMoment = startDateMoment.clone().add(13, "days");
-      const startDate = startDateMoment.format(DATE_FORMAT_HAUKI);
-      const endDate = endDateMoment.format(DATE_FORMAT_HAUKI);
+      const thisMondayMoment = moment().isoWeekday(1);
+      setStartDateMoment(thisMondayMoment);
+      const nextSundayMoment = thisMondayMoment.clone().add(13, "days");
+      const startDate = thisMondayMoment.format(DATE_FORMAT_HAUKI);
+      const endDate = nextSundayMoment.format(DATE_FORMAT_HAUKI);
       const openingTimesResponse = await fetch(
         `${getOrigin(router)}/api/openingtimes/get/${openingTimesId}/?start_date=${startDate}&end_date=${endDate}`
       );
@@ -42,6 +44,49 @@ const OpeningTimesInfo = (): ReactElement => {
   const useMountEffect = (fun: () => void) => useEffect(fun, []);
   useMountEffect(getOpeningTimesOnMount);
 
+  const renderOpeningTimes = (offsetDays: number) => {
+    if (startDateMoment) {
+      // Loop through the days of the week, using the offset for next week's times
+      return Array.from(Array(7).keys()).map((dayOfWeek) => {
+        const weekday = dayOfWeek + offsetDays;
+
+        const weekdayMoment = startDateMoment.clone().add(weekday, "days");
+        const weekdayOpeningTimes = openingTimes.find((openingTime) => {
+          const { date = "" } = openingTime || {};
+          const dateMoment = moment(date, DATE_FORMAT_HAUKI);
+          return dateMoment.isSame(weekdayMoment, "day");
+        });
+        const { times = [] } = weekdayOpeningTimes || {};
+        const dayKey = `openingtime_${weekday}`;
+
+        return (
+          <div key={dayKey} className={`${styles.gridContent}`}>
+            <div className={styles.weekday}>{`${i18n.t(`common.weekDay.${weekdayMoment.isoWeekday()}`)} ${weekdayMoment.format(DATE_FORMAT)}`}</div>
+
+            {times.length === 0 ? (
+              <div>{i18n.t("notification.opening.times.noTimes")}</div>
+            ) : (
+              times.map((time, index) => {
+                const { start_time, end_time, resource_state } = time;
+                const timeKey = `openingtime_${weekday}_${index}`;
+
+                return (
+                  <div key={timeKey}>
+                    {`${moment(start_time, TIME_FORMAT_HAUKI).format(TIME_FORMAT)} - ${moment(end_time, TIME_FORMAT_HAUKI).format(
+                      TIME_FORMAT
+                    )} ${i18n.t(`notification.opening.resourceState.${resource_state}`)}`}
+                  </div>
+                );
+              })
+            )}
+          </div>
+        );
+      });
+    }
+
+    return <></>;
+  };
+
   return !openingTimes || openingTimes.length === 0 ? (
     <></>
   ) : (
@@ -49,45 +94,17 @@ const OpeningTimesInfo = (): ReactElement => {
       <h2>{i18n.t("notification.opening.title")}</h2>
 
       <div className={`gridLayoutContainer ${styles.results}`}>
-        <div className={`${styles.gridColumn1} ${styles.gridHeader}`}>
-          <div className={styles.flexItem}>{i18n.t("notification.opening.times.date")}</div>
-        </div>
-        <div className={`${styles.gridColumn2} ${styles.gridHeader}`}>
-          <div className={styles.flexItem}>{i18n.t("notification.opening.times.opens")}</div>
-        </div>
-        <div className={`${styles.gridColumn3} ${styles.gridHeader}`}>
-          <div className={styles.flexItem}>{i18n.t("notification.opening.times.closes")}</div>
-        </div>
-        <div className={`${styles.gridColumn4} ${styles.gridHeader}`}>
-          <div className={styles.flexItem}>{i18n.t("notification.opening.times.status")}</div>
+        <div className={`${styles.gridColumn1}`}>
+          <div className={`${styles.gridHeader}`}>{i18n.t("notification.opening.times.thisWeek")}</div>
+
+          {renderOpeningTimes(0)}
         </div>
 
-        {openingTimes.map((openingTime, index1) => {
-          const { date = "", times = [] } = openingTime || {};
-          const dateMoment = moment(date, DATE_FORMAT_HAUKI);
+        <div className={`${styles.gridColumn2}`}>
+          <div className={`${styles.gridHeader}`}>{i18n.t("notification.opening.times.nextWeek")}</div>
 
-          return times.map((time, index2) => {
-            const { start_time, end_time, resource_state } = time;
-            const key2 = `openingtime_${index1}_${index2}`;
-
-            return (
-              <Fragment key={key2}>
-                <div className={`${styles.gridColumn1} ${styles.gridContent}`}>
-                  <div className={styles.flexItem}>{`${i18n.t(`common.weekDay.${dateMoment.isoWeekday()}`)} ${dateMoment.format(DATE_FORMAT)}`}</div>
-                </div>
-                <div className={`${styles.gridColumn2} ${styles.gridContent}`}>
-                  <div className={styles.flexItem}>{moment(start_time, TIME_FORMAT_HAUKI).format(TIME_FORMAT)}</div>
-                </div>
-                <div className={`${styles.gridColumn3} ${styles.gridContent}`}>
-                  <div className={styles.flexItem}>{moment(end_time, TIME_FORMAT_HAUKI).format(TIME_FORMAT)}</div>
-                </div>
-                <div className={`${styles.gridColumn4} ${styles.gridContent}`}>
-                  <div className={styles.flexItem}>{resource_state}</div>
-                </div>
-              </Fragment>
-            );
-          });
-        })}
+          {renderOpeningTimes(7)}
+        </div>
       </div>
     </div>
   );
